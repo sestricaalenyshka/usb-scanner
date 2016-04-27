e863dec75c59b1cd80aa9201613253ceb74667cf
sestricaalenyshka <sestica@inbox.ru>
2016-04-27 21:52:57 +0300

    Добавлен класс для работы с базой данных

---
+++ brain-server/src/com/github/ergpscanner/brainserver/db/DbHelper.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2016 sestricaalenyshka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.github.ergpscanner.brainserver.db;
+
+import com.github.ergpscanner.brainserver.model.Folder;
+
+import java.sql.*;
+
+/**
+ * Набор утилитарных методов для работы с базами данных.
+ *
+ * @author sestricaalenyshka
+ */
+public class DbHelper {
+    private String url;
+    private String userName;
+    private String password;
+    private String driverClassName;
+
+    public DbHelper(String driverClassName, String url, String userName, String password) {
+        this.url = url;
+        this.userName = userName;
+        this.password = password;
+        this.driverClassName = driverClassName;
+    }
+
+    public Folder getFolder(String folderName) throws SQLException, ClassNotFoundException {
+
+        try (Connection con = getConnection();
+             PreparedStatement stmt = con.prepareStatement("select * from df_folders where no = ?")) {
+            stmt.setString(1, folderName);
+
+            try (ResultSet resultSet = stmt.executeQuery()) {
+                if (resultSet.next()) {
+                    Folder folder = new Folder();
+                    folder.setName(resultSet.getString("no"));
+                    folder.setCreateDate(resultSet.getDate("s_date"));
+                    return folder;
+                }
+            }
+        }
+        return null;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName(driverClassName);
+        Connection conn = DriverManager.getConnection(url, userName, password);
+        conn.setReadOnly(true);
+        return conn;
+    }
+}
---
+++ brain-server/test/com/github/ergpscanner/brainserver/db/DbHelperTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2016 sestricaalenyshka
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.github.ergpscanner.brainserver.db;
+
+import com.github.ergpscanner.brainserver.model.Folder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author sestricaalenyshka
+ */
+public class DbHelperTest {
+
+    private static final String TEST_DB_DRIVER = "org.h2.Driver";
+    private static final String TEST_DB_URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        Class.forName(TEST_DB_DRIVER);
+        try (Connection conn = DriverManager.getConnection(TEST_DB_URL, "", "");
+             Statement stmt = conn.createStatement()) {
+            stmt.execute("create table df_folders (no varchar(255), s_date date)");
+            stmt.execute("insert into df_folders (no, s_date) values ('123', '2015-04-27')");
+        }
+    }
+
+    @Test
+    public void getFolderNull() throws Exception {
+        DbHelper dbHelper = new DbHelper(TEST_DB_DRIVER, TEST_DB_URL, "", "");
+        assertNull(dbHelper.getFolder("321"));
+    }
+
+    @Test
+    public void getFolderName() throws Exception {
+        DbHelper dbHelper = new DbHelper(TEST_DB_DRIVER, TEST_DB_URL, "", "");
+        Folder folder = dbHelper.getFolder("123");
+        assertEquals("123", folder.getName());
+    }
+
+    @Test
+    public void getFolderNotNull() throws Exception {
+        DbHelper dbHelper = new DbHelper(TEST_DB_DRIVER, TEST_DB_URL, "", "");
+        assertNotNull(dbHelper.getFolder("123"));
+    }
+
+}
